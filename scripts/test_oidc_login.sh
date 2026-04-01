8ad47620ea94c97d3913be2ff6008566e6b6dbe1
Fabio Berchtold <fabio.berchtold@swisscom.com>
2026-04-01 13:00:52 +0200

    fix quotes

--- scripts/test_oidc_login.sh
+++ scripts/test_oidc_login.sh
@@ -10,22 +10,22 @@ PASSWORD="relative"
 
 # Fetch client credentials from the broker
 echo "Fetching client credentials from broker ..."
-BINDING=$(curl -sf "$BROKER_URL/v2/service_instances/$INSTANCE_ID/service_bindings/db59931a-70a6-43c1-8885-b0c6b1c194d4" -X PUT)
+BINDING=$(curl -sf "$BROKER_URL/v2/service_instances/$INSTANCE_ID/service_bindings/db59931a-70a6-43c1-8885-b0c6b1c194d4")
 CLIENT_ID=$(echo "$BINDING" | jq -r '.credentials.clientId')
 CLIENT_SECRET=$(echo "$BINDING" | jq -r '.credentials.clientSecret')
 echo "Client ID:     $CLIENT_ID"
 echo "Client Secret: $CLIENT_SECRET"
 echo ""
 
-# Login with test user via "password" grant
+# Login with test user via Resource Owner Password Credentials grant
 echo "Logging in as $USERNAME ..."
-TOKEN_RESPONSE=$(curl -sfv -X POST "$KEYCLOAK_URL/realms/$REALM/protocol/openid-connect/token" \
-  -d "grant_type='password'" \
-  -d "client_id='$CLIENT_ID'" \
-  -d "client_secret='$CLIENT_SECRET'" \
-  -d "username='$USERNAME'" \
-  -d "password='$PASSWORD'" \
-  -d "scope='openid'")
+TOKEN_RESPONSE=$(curl -sf -X POST "$KEYCLOAK_URL/realms/$REALM/protocol/openid-connect/token" \
+  -d "grant_type=password" \
+  -d "client_id=$CLIENT_ID" \
+  -d "client_secret=$CLIENT_SECRET" \
+  -d "username=$USERNAME" \
+  -d "password=$PASSWORD" \
+  -d "scope=openid")
 ACCESS_TOKEN=$(echo "$TOKEN_RESPONSE" | jq -r '.access_token')
 echo "Access token obtained: [$ACCESS_TOKEN]"
 echo ""
